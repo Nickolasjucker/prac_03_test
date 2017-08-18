945515b4c137bf8e3d348f9497491d5a1ea6c269
jc468021 <nickolas.jucker@my.jcu.edu.au>
2017-08-18 11:13:07 +1000

    add name error checking

--- oddName.py
+++ oddName.py
@@ -1,1 +1,18 @@
 """Nickolas Jucker"""
+def get_name(name):
+    correct_input = True
+    while correct_input:
+        if len(name) > 0:
+            remove_space = name.strip()
+            name_length = len(remove_space)
+            print(remove_space[1:name_length:2])
+            correct_input = False
+        else:
+            print("Invalid input your name.")
+            name = input("What is your name?: ")
+def main():
+    name = input("What is your name?: ")
+    get_name(name)
+
+
+main()
